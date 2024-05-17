0a0e17729d376293c6a2dc9cddc9f8768f8e1dd9
Gabriel Dominguez Duran <gabrieldominguezduran@gmail.com>
2024-05-17 10:54:56 +0200

    Update astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/noop",
+    },
+  },
+});
