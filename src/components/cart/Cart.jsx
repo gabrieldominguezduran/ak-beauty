46897c9191a6af60211147f052272c0d1023792d
Gabriel Dominguez Duran <gabrieldominguezduran@gmail.com>
2024-06-03 17:11:20 +0200

    Update img src in the cart

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -3,6 +3,8 @@ import { useEffect } from "preact/hooks";
 import { isCartOpen, cartItems, removeCartItem } from "../../stores/cartStore";
 import "./cartStyles.css";
 
+const images = import.meta.glob("/src/images/*.{jpeg,jpg,png,gif}");
+
 export default function Cart() {
   const $isCartOpen = useStore(isCartOpen);
   const $cartItems = useStore(cartItems);
@@ -21,19 +23,23 @@ export default function Cart() {
         ×
       </button>
       <ul>
-        {Object.values($cartItems).map((cartItem) => (
-          <li key={cartItem.id}>
-            <img src={cartItem.image} alt={cartItem.name} width="50" />
-            <h4>{cartItem.name}</h4>
-            <p>{cartItem.quantity}</p>
-            <button
-              className="trash-btn"
-              onClick={() => removeCartItem(cartItem.id)}
-            >
-              <i class="fa-regular fa-trash-can"></i>{" "}
-            </button>
-          </li>
-        ))}
+        {Object.values($cartItems).map((cartItem) => {
+          const imgSrc = Object.keys(images).find((key) =>
+            key.includes(cartItem.image)
+          );
+
+          console.log(imgSrc);
+          return (
+            <li key={cartItem.id}>
+              <img src={imgSrc} alt={cartItem.name} width="50" />
+              <h3>{cartItem.name}</h3>
+              <p>Quantity: {cartItem.quantity}</p>
+              <button onClick={() => removeCartItem(cartItem.id)}>
+                Delete
+              </button>
+            </li>
+          );
+        })}
       </ul>
       {Object.values($cartItems).length === 0 && <p>Your cart is empty!</p>}
     </aside>
